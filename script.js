61c8712a070a6be50302bbb98867c3911c300bb8
GarbanzoBeanz <150662238+GarbanzoBeanz@users.noreply.github.com>
2025-05-01 11:44:17 +0800

    Write pseudocode of handling number logic
    the waitingForSecondOperand makes sense so that users can input numbers
    after an operator without the numbers just appending. we also don't want
    the first input to append to 0 when displaying the value.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 // ==== Initialize ==== //
 // displayValue = 0
+// firstOperand is empty
+// secondOperand is empty
+// waitingForSecondOperand is false
 
 // ==== Event Listeners ==== //
 // for each button:
@@ -10,4 +13,15 @@
 //  if the button is clear-all (AC) then call resetCalculator()
 //  if the button is clear (C) then call deleteLastDigit()
 //  if the button is a decimal or parenthesis call addSymbol(value)
+
+// ==== handleNumInput ==== //
+// if waitingForSecondOperand is true
+//  displayValue = clickedNumber
+//  waitingForSecondOperand = false
+// else
+//    if displayValue is 0
+//      displayValue = 0
+//    else
+//      append clicked number to displayValue
+// update the display
 
