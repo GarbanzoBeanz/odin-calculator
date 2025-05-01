e24bf1a4eac452490e6afb40c72ac07148bd1d8d
GarbanzoBeanz <150662238+GarbanzoBeanz@users.noreply.github.com>
2025-05-01 11:06:37 +0800

    Write pseudocode for event listeners
    This allows me outline what I need the system to do and also helps plan
    how to tackle this project.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// ==== Event Listeners ==== //
+// for each button:
+//  if the button is a number then call handleNumInput(value)
+//  if button is an operator then call handleOperator(operator)
+//  if the button is equals then call evaluateFormula()
+//  if the button is clear-all (AC) then call resetCalculator()
+//  if the button is clear (C) then call deleteLastDigit()
+//  if the button is a decimal or parenthesis call addSymbol(value)
+
